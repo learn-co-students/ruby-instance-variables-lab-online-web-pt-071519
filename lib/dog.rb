1309450feff118180cde74a3850ee5d59e5fcdab
Renee Sims <info@reneesoul.com>
2019-08-06 05:57:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name=(dog_name)   # writer
+    @this_dogs_name = dog_name
+  end
+  
+  def name              # reader
+    @this_dogs_name
+  end
+end
+
+
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+lassie.name
